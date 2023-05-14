bc511fb103dbd4939a6c7d7b4e808d8ea44aa186
Fermin Perez <fermin_05@hotmail.com>
2023-05-14 19:48:10 -0300

    First Commit

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "54d7d7dce1msh81566f61eb7d7d9p17363cjsnd96288ea8b52",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "4a549f0cff988b8090dd0efe805c9a44";
